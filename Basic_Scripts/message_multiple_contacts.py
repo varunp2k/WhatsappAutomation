149a1000b2cf89abcecd4a5d282e995dbd155169
bhargavsk1077 <51043479+bhargavsk1077@users.noreply.github.com>
2019-10-06 18:48:24 +0530

    Update message_multiple_contacts.py

--- Basic_Scripts/message_multiple_contacts.py
+++ Basic_Scripts/message_multiple_contacts.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.support import expected_conditions as EC
+
 
 driver = webdriver.Chrome(r"add path to chrome driver here")
 driver.get('https://web.whatsapp.com/')
@@ -25,3 +27,11 @@ for i in contacts:
     count = 1
     send_msg(driver, name, msg, count)
     time.sleep(1.5)
+    
+    
+options = driver.find_element_by_xpath('//div[@title="Menu"]')
+options.click()
+logout_xpath = '/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[3]/span/div/ul/li[6]/div'
+WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, logout_xpath)))
+logout = driver.find_element_by_xpath(logout_xpath)
+logout.click()
